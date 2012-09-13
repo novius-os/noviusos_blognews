fe6549c80a308dbd674ae5eab5db45ccad429fa2
Gilles FELIX <felix@novius.com>
2012-09-13 09:56:31 +0200

    Cleaning metadata

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -1,9 +1,8 @@
 <?php
 return array(
-    'name'    => '_blognews (required for blog or news)',
-    'version' => '0.02-beta203',
+    'name'    => 'Blog / News (required for Blog or News)',
+    'version' => '0.01',
     'href' => 'admin/noviusos_blognews/appdesk',
-    'icon64'  => 'static/apps/noviusos_blognews/img/64/blog.png',
     'provider' => array(
         'name' => 'Novius Dev',
     ),
