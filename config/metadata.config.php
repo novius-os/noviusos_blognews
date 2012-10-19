70514b0970253d78de73955b5623638a4984af23
Gilles FELIX <felix@novius.com>
2012-10-19 11:24:00 +0200

    "Applications : change metadata version to 0.2"

--- config/metadata.config.php
+++ config/metadata.config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'name'    => 'Blog / News (required for Blog or News)',
-    'version' => '0.1',
+    'version' => '0.2',
     'provider' => array(
         'name' => 'Novius Dev',
     ),
