7523dfe70e8a38bc099da03e290e62c01bb3e81b
Sébastien Drouyer <drouyer@novius.com>
2012-11-15 19:13:45 +0100

    moved configuration to common

--- config/controller/admin/appdesk.config.php
+++ config/controller/admin/appdesk.config.php
@@ -10,10 +10,7 @@ return array(
         'date'
     ),
     'toolbar' => array(
-        'actions' => array(
-            'Nos\User\Model_User.add' => false,
-            '{{namespace}}\Model_Tag.add' => false,
-        )
+        'models' => array('{{namespace}}\Model_Post', '{{namespace}}\Model_Category')
     ),
     'query' => array(
         'model' => '{{namespace}}\Model_Post',
@@ -21,34 +18,4 @@ return array(
         'limit' => 20,
     ),
     'search_text' => 'post_title',
-    'dataset' => array(
-        'title' => array(
-            'column' => 'post_title',
-            'headerText'    => __('Title')
-        ),
-        'context' => true,
-        'author' => array(
-            'headerText'    => __('Author'),
-            'search_relation' => 'author',
-            'search_column' => 'author.user_name',
-            'value' => function ($item) {
-                return $item->author->fullname();
-            },
-        ),
-        'publication_status' => true,
-        'post_created_at' => array(
-            'headerText'    => __('Date'),
-            'search_column' => 'post_created_at',
-            'dataType' => 'datetime',
-            'value' => function ($item) {
-                return \Date::create_from_string($item->post_created_at, 'mysql')->format('%m/%d/%Y %H:%M:%S'); //%m/%d/%Y %H:%i:%s
-            },
-        ),
-        'preview_url' => array(
-            'value' => function($item) {
-                return $item->preview_url();
-            },
-            'visible' => false
-        ),
-    ),
 );
