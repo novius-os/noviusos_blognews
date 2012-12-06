b7cbde472c2a1a9baaece43d4a0f102bea432c58
Gilles FELIX <felix@novius.com>
2012-12-06 15:28:56 +0100

    "Refactoring enhancer popup with new core system"

---
+++ config/controller/admin/application.config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+
+return array(
+    'popup' => array(
+        'layout' => array(
+            'view' => 'noviusos_blognews::admin/application/popup',
+        ),
+    ),
+    'preview' => array(
+        'params' => array(
+            'icon' => 'static/apps/{{application_name}}/img/{{icon}}-64.png',
+            'title' => 'Blog / News',
+        ),
+    ),
+);
--- views/admin/application/preview.view.php
+++
@@ -1,4 +0,0 @@
-<div style="overflow: hidden">
-    <img style="display: block; float: left; width: 64px; height: 64px;" src="<?= \Config::icon($app_config['application_name'], 64) ?>" />
-    <h1 style="margin-left: 80px;"><?= e($app_config['application_label']) ?></h1>
-</div>
