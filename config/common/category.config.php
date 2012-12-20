e6b29f9401857ab48e71a5c8e4552bc301523ca5
esperat <esperat@novius.fr>
2012-12-20 10:52:04 +0100

    "Copy review uniformisation"

--- config/common/category.config.php
+++ config/common/category.config.php
@@ -18,24 +18,24 @@ return array(
     ),
     'i18n' => array(
         // Crud
-        'successfully added' => __('Category successfully added.'),
-        'successfully saved' => __('Category successfully saved.'),
-        'successfully deleted' => __('The category has successfully been deleted!'),
+        'successfully added' => __('Done! The category has been added.'),
+        'successfully deleted' => __('The category has been deleted.'),
 
         // General errors
-        'item deleted' => __('This category has been deleted.'),
-        'not found' => __('Category not found'),
+        'item deleted' => __('This category doesn’t exist any more. It has been deleted.'),
+        'not found' => __('We cannot find this category.'),
 
         // Blank slate
-        'error added in context' => __('This category cannot be added {context}.'),
-        'item inexistent in context yet' => __('This category has not been added in {context} yet.'),
-        'add an item in context' => __('Add a new category in {context}'),
+        'error added in context not parent' => __('We’re afraid this category cannot be added in {{context}} because its <a>parent</a> is not available in this context yet.'), #wtf two strings needed here (this context / this language)
 
         // Deletion popup
-        'delete an item' => __('Delete a category'),
-        'you are about to delete' => __('You are about to delete the category <span style="font-weight: bold;">":title"</span>.'),
-        'you are about to delete, confim' => __('You are about to delete the category <span style="font-weight: bold;">":title"</span>. Are you sure you want to continue?'),
-        'exists in multiple context' => __('This category exists in <strong>{count} contexts</strong>.'),
+        'delete an item' => __('Deleting the category ‘{{title}}’'),
+        'you are about to delete, confim' => __('Last chance, there’s no undo. Do you really want to delete this category?'),
+        'exists in multiple context' => __('This category exists in <strong>{{count}} contexts</strong>.'),
         'delete in the following contexts' => __('Delete this category in the following contexts:'),
+        'item has 1 sub-item' => __('This category has <strong>1 sub-category</strong>.'),
+        'item has multiple sub-items' => __('This category has <strong>{{count}} sub-category</strong>.'),
+        'yes delete sub-items' => __('Yes, I want to delete this category and all of its {{count}} sub-category.'),
+        'confirm deletion wrong_confirmation' => __('We cannot delete this category as the number of sub-items you’ve entered is wrong. Please amend it.'),
     ),
 );
--- config/common/post.config.php
+++ config/common/post.config.php
@@ -47,24 +47,17 @@ return array(
     ),
     'i18n' => array(
         // Crud
-        'successfully added' => __('Post successfully added.'),
-        'successfully saved' => __('Post successfully saved.'),
-        'successfully deleted' => __('The post has successfully been deleted!'),
+        'successfully added' => __('Done! The post has been added.'),
+        'successfully deleted' => __('The post has been deleted.'),
 
         // General errors
-        'item deleted' => __('This post has been deleted.'),
-        'not found' => __('Post not found'),
-
-        // Blank slate
-        'error added in context' => __('This post cannot be added {context}.'),
-        'item inexistent in context yet' => __('This post has not been added in {context} yet.'),
-        'add an item in context' => __('Add a new post in {context}'),
+        'item deleted' => __('This post doesn’t exist any more. It has been deleted.'),
+        'not found' => __('We cannot find this post.'),
 
         // Deletion popup
-        'delete an item' => __('Delete a post'),
-        'you are about to delete, confim' => __('You are about to delete the post <span style="font-weight: bold;">":title"</span>. Are you sure you want to continue?'),
-        'you are about to delete' => __('You are about to delete the post <span style="font-weight: bold;">":title"</span>.'),
-        'exists in multiple context' => __('This post exists in <strong>{count} contexts</strong>.'),
+        'delete an item' => __('Deleting the post ‘{{title}}’'),
+        'you are about to delete, confim' => __('Last chance, there’s no undo. Do you really want to delete this post?'),
+        'exists in multiple context' => __('This post exists in <strong>{{count}} contexts</strong>.'),
         'delete in the following contexts' => __('Delete this post in the following contexts:'),
     ),
     'actions' => array(
