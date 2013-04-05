50e6529a4a1efa62811a4fb0ede08bc9e80e4587
Gilles FELIX <felix@novius.com>
2013-04-05 10:36:13 +0200

    Add author in post display

--- views/front/post/show.view.php
+++ views/front/post/show.view.php
@@ -13,6 +13,7 @@
 
     <?= \View::forge('noviusos_blognews::front/post/thumbnail', array('item' => $item), false) ?>
     <?= \View::forge('noviusos_blognews::front/post/title', array('item' => $item, 'context' => 'show'), false) ?>
+    <?= \View::forge('noviusos_blognews::front/post/author', array('item' => $item), false) ?>
     <?= \View::forge('noviusos_blognews::front/post/publication_date', array('item' => $item), false) ?>
 
     <?= \View::forge('noviusos_blognews::front/post/summary', array('item' => $item), false) ?>
