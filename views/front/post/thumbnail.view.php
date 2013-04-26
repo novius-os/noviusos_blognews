d355d75ed77715d80812d8def9d60fafbe53ae4b
Julian Esperat <esperat@novius.fr>
2013-04-26 16:28:06 +0200

    Thumbnail configuration: size of thumbnail + link on thumbnail

--- views/front/post/thumbnail.view.php
+++ views/front/post/thumbnail.view.php
@@ -8,8 +8,31 @@
  * @link http://www.novius-os.org
  */
 
-if (isset($item->medias->thumbnail)) {
-    ?>
-    <img src="<?= $item->medias->thumbnail->get_public_path_resized(200) ?>" class="blognews_thumbnail" />
-    <?php
+
+$context = empty($context) || $context != 'show' ? 'list' : 'item';
+
+if ($context == 'list') {
+    $print_link = \Arr::get($blognews_config, 'thumbnail.front.list.link_to_item', false);
+    $thumbnail_width = \Arr::get($blognews_config, 'thumbnail.front.list.max_width', 120);
+    $thumbnail_height = \Arr::get($blognews_config, 'thumbnail.front.list.max_height', $thumbnail_width);
+
+    if (isset($item->medias->thumbnail)) {
+        $print_link and print '<a href="'.$item->url().'" class="blognews_thumbnail_link">';
+        ?>
+        <img src="<?= $item->medias->thumbnail->get_public_path_resized($thumbnail_width, $thumbnail_height) ?>" class="blognews_thumbnail" />
+        <?php
+        $print_link and print '</a>';
+    }
+} else {
+    $print_link = \Arr::get($blognews_config, 'thumbnail.front.item.link_to_fullsize', true);
+    $thumbnail_width = \Arr::get($blognews_config, 'thumbnail.front.item.max_width', 200);
+    $thumbnail_height = \Arr::get($blognews_config, 'thumbnail.front.list.max_height', $thumbnail_width);
+
+    if (isset($item->medias->thumbnail)) {
+        $print_link and print '<a href="'.$item->medias->thumbnail->get_public_path().'" class="blognews_thumbnail_link_fullsize">';
+        ?>
+        <img src="<?= $item->medias->thumbnail->get_public_path_resized($thumbnail_width, $thumbnail_height) ?>" class="blognews_thumbnail" />
+        <?php
+        $print_link and print '</a>';
+    }
 }
