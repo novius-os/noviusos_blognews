a55e99d8c3a14b3587d41c55253ce2ca16dbf56e
Julian Esperat <esperat@novius.fr>
2013-04-26 16:56:23 +0200

    Easier code

--- views/front/post/thumbnail.view.php
+++ views/front/post/thumbnail.view.php
@@ -8,31 +8,29 @@
  * @link http://www.novius-os.org
  */
 
+if (isset($item->medias->thumbnail)) {
 
-$context = empty($context) || $context != 'show' ? 'list' : 'item';
+    $context = empty($context) || $context != 'show' ? 'list' : 'item';
 
-if ($context == 'list') {
-    $print_link = \Arr::get($blognews_config, 'thumbnail.front.list.link_to_item', false);
-    $thumbnail_width = \Arr::get($blognews_config, 'thumbnail.front.list.max_width', 120);
-    $thumbnail_height = \Arr::get($blognews_config, 'thumbnail.front.list.max_height', $thumbnail_width);
+    if ($context == 'list') {
+        $print_link     = \Arr::get($blognews_config, 'thumbnail.front.list.link_to_item', false);
+        $thumbnail_width = \Arr::get($blognews_config, 'thumbnail.front.list.max_width', 120);
+        $thumbnail_height = \Arr::get($blognews_config, 'thumbnail.front.list.max_height', $thumbnail_width);
 
-    if (isset($item->medias->thumbnail)) {
-        $print_link and print '<a href="'.$item->url().'" class="blognews_thumbnail_link">';
-        ?>
-        <img src="<?= $item->medias->thumbnail->get_public_path_resized($thumbnail_width, $thumbnail_height) ?>" class="blognews_thumbnail" />
-        <?php
-        $print_link and print '</a>';
+        $link_url   = $item->url();
+        $link_class = 'blognews_thumbnail_link';
+        $img_src    = $item->medias->thumbnail->get_public_path_resized($thumbnail_width, $thumbnail_height);
+    } else {
+        $print_link = \Arr::get($blognews_config, 'thumbnail.front.item.link_to_fullsize', true);
+        $thumbnail_width = \Arr::get($blognews_config, 'thumbnail.front.item.max_width', 200);
+        $thumbnail_height = \Arr::get($blognews_config, 'thumbnail.front.list.max_height', $thumbnail_width);
+
+        $link_url   = $item->medias->thumbnail->get_public_path();
+        $link_class = 'blognews_thumbnail_link_fullsize';
+        $img_src    = $item->medias->thumbnail->get_public_path_resized($thumbnail_width, $thumbnail_height);
     }
-} else {
-    $print_link = \Arr::get($blognews_config, 'thumbnail.front.item.link_to_fullsize', true);
-    $thumbnail_width = \Arr::get($blognews_config, 'thumbnail.front.item.max_width', 200);
-    $thumbnail_height = \Arr::get($blognews_config, 'thumbnail.front.list.max_height', $thumbnail_width);
 
-    if (isset($item->medias->thumbnail)) {
-        $print_link and print '<a href="'.$item->medias->thumbnail->get_public_path().'" class="blognews_thumbnail_link_fullsize">';
-        ?>
-        <img src="<?= $item->medias->thumbnail->get_public_path_resized($thumbnail_width, $thumbnail_height) ?>" class="blognews_thumbnail" />
-        <?php
-        $print_link and print '</a>';
-    }
+    $print_link and print '<a href="'.$link_url.'" class="'.$link_class.'">';
+    echo '<img src="'.$img_src.'" class="blognews_thumbnail" />';
+    $print_link and print '</a>';
 }
