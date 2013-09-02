8f74fdf7640380160869f127aeadcbf00eb305aa
Gilles FELIX <felix@novius.com>
2013-09-02 14:02:43 +0200

    Remove unsed code

--- config/controller/front.config.php
+++ config/controller/front.config.php
@@ -9,7 +9,6 @@
  */
 
 return array(
-    'date_format'   => '%A %e %B %Y',
     'link_on_title' => false,
     'item_per_page' => 10,
     'order_by'    => array('post_created_at' => 'DESC', 'post_id' => 'DESC'),
